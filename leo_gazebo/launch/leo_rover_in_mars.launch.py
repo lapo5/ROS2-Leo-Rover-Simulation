9fa5bb8f0ba2fd9db55057f6c4df8a0ea5795901
marcolapo5 <marco.lapolla5@gmail.com>
2022-07-07 14:08:39 +0200

    Update

---
+++ leo_gazebo/launch/leo_rover_in_mars.launch.py
@@ -0,0 +1,73 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.actions import ExecuteProcess, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+import xacro
+
+
+def generate_launch_description():
+
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_leo_description = get_package_share_directory('leo_rover_description')
+    pkg_leo_gazebo_worlds = get_package_share_directory('leo_gazebo_worlds')
+    pkg_ezrassor_worlds = get_package_share_directory('ezrassor_sim_gazebo')
+
+
+    # Set the path to the world file
+    world_file_name = 'marsyard2021.world'
+    world_path = os.path.join(pkg_leo_gazebo_worlds, 'worlds', world_file_name)
+   
+    declare_world_cmd = DeclareLaunchArgument(
+        name='world',
+        default_value=world_path,
+        description='Full path to the world model file to load')
+
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    )
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    urdf_file_name = 'urdf/leo_rover.urdf'
+
+    urdf = os.path.join(
+      pkg_leo_description,
+      urdf_file_name)
+
+    print("urdf_file_name : {}".format(urdf))
+
+    robot_state_pub = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=[urdf])
+
+
+    # GAZEBO_MODEL_PATH has to be correctly set for Gazebo to be able to find the model
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-entity', 'leo_rover', '-topic', '/robot_description'],
+                        output='screen')
+
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        declare_world_cmd,
+        gazebo,
+        robot_state_pub,
+        spawn_entity
+  ])
